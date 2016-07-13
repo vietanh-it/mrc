90d1f7862acbe6db25be5ad679e8dfd7cb92e2fb
Viet Anh <vietanh.sgu@gmail.com>
2016-07-13 14:32:02 +0700

    update

---
+++ wp-content/plugins/ringier-v1/app/Controllers/JourneyController.php
@@ -0,0 +1,26 @@
+<?php
+namespace RVN\Controllers;
+
+class JourneyController extends _BaseController
+{
+    private static $instance;
+
+    protected function __construct()
+    {
+        parent::__construct();
+    }
+
+    public static function init()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new JourneyController();
+        }
+
+        return self::$instance;
+    }
+
+    public function getJourneyDetail($journey_id)
+    {
+
+    }
+}
--- wp-content/plugins/ringier-v1/app/Hooks/CustomShips.php
+++ wp-content/plugins/ringier-v1/app/Hooks/CustomShips.php
@@ -3,6 +3,7 @@ namespace RVN\Hooks;
 
 use RVN\Library\CPTColumns;
 use RVN\Models\Posts;
+
 /**
  * Created by PhpStorm.
  *
@@ -23,89 +24,93 @@ class CustomShips
     }
 
 
-    public function __construct(){
+    public function __construct()
+    {
         global $wpdb;
 
         $this->_table_size = $wpdb->prefix . "ship_info";
 
-        add_action( 'add_meta_boxes', array($this,'add_product_size') );
+        add_action('add_meta_boxes', array($this, 'add_product_size'));
         add_action('save_post', array($this, 'save'));
     }
 
-    public function add_product_size() {
-        add_meta_box('ship_detail', 'Ship room', array($this,'show'), 'ship', 'normal', 'high');
+    public function add_product_size()
+    {
+        add_meta_box('ship_detail', 'Ship room', array($this, 'show'), 'ship', 'normal', 'high');
     }
 
 
-    public function show() {
+    public function show()
+    {
         global $post;
         $ship_info = $this->getShipInfo($post->ID);
         $result = array();
-        if($ship_info && $ship_info->room_types){
+        if ($ship_info && $ship_info->room_types) {
             $result = unserialize($ship_info->room_types);
         }
 
         echo '<div class = "ctn-box-size">';
-        if($result){
-            foreach ($result as $v){
+        if ($result) {
+            foreach ($result as $v) {
                 $stateroom_type = $v['room_type'];
                 $quantity = $v['quantity'];
-                echo  $this->html_size($stateroom_type,$quantity);
+                echo $this->html_size($stateroom_type, $quantity);
             }
-        }else{
-            echo  $this->html_size();
+        } else {
+            echo $this->html_size();
         }
         echo '</div>';
         echo '
             <a href = "javascript:void(0)" class="add-new-size" data-id="2">Add new STATEROOM TYPE</a>
         ';
 
-         ?>
-            <script>
-                var $ = jQuery.noConflict();
+        ?>
+        <script>
+            var $ = jQuery.noConflict();
 
-                jQuery(document).ready(function ($) {
+            jQuery(document).ready(function ($) {
 
-                    var html_size = '<div class ="size-product" style="margin-bottom: 30px;border-bottom: 1px solid darkgrey;padding-bottom: 20px"> ' +
-                        '<div class="size" style="float: left;width: 50%"> ' +
-                        '<label >ROOM TYPE : </label> ' +
-                        '<input type="text" value="" name="size[]"> ' +
-                        '</div> ' +
-                        '<div class ="quantity" style="float: left;width: 50%;    margin-bottom: 10px;"> ' +
-                        '<label >Quantity : </label> ' +
-                        '<input type="number" value="" name="quantity_size[]" > ' +
-                        '</div> ' +
-                        '<div class="close" style=""> ' +
-                        '<a href="javascript:void(0)" class="close-box-size" >Delete</a> ' +
-                        '</div>' +
-                        '</div>' ;
+                var html_size = '<div class ="size-product" style="margin-bottom: 30px;border-bottom: 1px solid darkgrey;padding-bottom: 20px"> ' +
+                    '<div class="size" style="float: left;width: 50%"> ' +
+                    '<label >ROOM TYPE : </label> ' +
+                    '<input type="text" value="" name="size[]"> ' +
+                    '</div> ' +
+                    '<div class ="quantity" style="float: left;width: 50%;    margin-bottom: 10px;"> ' +
+                    '<label >Quantity : </label> ' +
+                    '<input type="number" value="" name="quantity_size[]" > ' +
+                    '</div> ' +
+                    '<div class="close" style=""> ' +
+                    '<a href="javascript:void(0)" class="close-box-size" >Delete</a> ' +
+                    '</div>' +
+                    '</div>';
 
-                    $('.add-new-size').click(function () {
-                        var id = $(this).attr('data-id');
+                $('.add-new-size').click(function () {
+                    var id = $(this).attr('data-id');
 
-                        $('.ctn-box-size').append(html_size);
-                    });
-
-                    $(document).delegate('.close-box-size','click',function () {
-                       $(this).closest('div').closest('div.size-product').remove();
-                    });
-
+                    $('.ctn-box-size').append(html_size);
                 });
 
-            </script>
+                $(document).delegate('.close-box-size', 'click', function () {
+                    $(this).closest('div').closest('div.size-product').remove();
+                });
+
+            });
+
+        </script>
 
         <?php
     }
 
-    public function html_size($stateroom_type = '',$quantity = ''){
+    public function html_size($stateroom_type = '', $quantity = '')
+    {
         $html = '<div class ="size-product" style="margin-bottom: 30px;border-bottom: 1px solid darkgrey;padding-bottom: 20px">
                     <div class="size" style="float: left;width: 50%">
                         <label >ROOM TYPE : </label>
-                        <input type="text" value="'.$stateroom_type.'" name="size[]">
+                        <input type="text" value="' . $stateroom_type . '" name="size[]">
                     </div>
                     <div class ="quantity" style="float: left;width: 50%;    margin-bottom: 10px;">
                         <label >Quantity : </label>
-                        <input type="number" value="'.$quantity.'" name="quantity_size[]" >
+                        <input type="number" value="' . $quantity . '" name="quantity_size[]" >
                     </div>
                      <div class="close" style="">
                         <a href="javascript:void(0)" class="close-box-size" >Delete</a>
@@ -116,15 +121,16 @@ class CustomShips
         return $html;
     }
 
-    public function save(){
-       // var_dump($_POST);
+    public function save()
+    {
+        // var_dump($_POST);
         global $wpdb;
-        if($_POST['size'] && $_POST['quantity_size']){
+        if ($_POST['size'] && $_POST['quantity_size']) {
             $list_size = array();
-            foreach ($_POST['size'] as $k => $stateroom_type){
+            foreach ($_POST['size'] as $k => $stateroom_type) {
                 $size_quantity = array(
                     'room_type' => $stateroom_type,
-                    'quantity' => $_POST['quantity_size'][$k],
+                    'quantity'  => $_POST['quantity_size'][$k],
                 );
 
                 $list_size[] = $size_quantity;
@@ -132,27 +138,28 @@ class CustomShips
 
 
             $ship_info = $this->getShipInfo($_POST['post_ID']);
-            if($ship_info){
-                $wpdb->update($this->_table_size,array('room_types' => serialize($list_size)),array('object_id' => $_POST['post_ID']));
-            }else{
-                $wpdb->insert($this->_table_size,array(
+            if ($ship_info) {
+                $wpdb->update($this->_table_size, array('room_types' => serialize($list_size)),
+                    array('object_id' => $_POST['post_ID']));
+            } else {
+                $wpdb->insert($this->_table_size, array(
                     'room_types' => serialize($list_size),
-                    'object_id' => $_POST['post_ID']
+                    'object_id'  => $_POST['post_ID']
                 ));
             }
 
         }
 
 
-
     }
 
-    public function getShipInfo($ship_id){
+    public function getShipInfo($ship_id)
+    {
         global $wpdb;
 
-        $query = 'SELECT * FROM '.$this->_table_size.' WHERE object_id = '.$ship_id;
+        $query = 'SELECT * FROM ' . $this->_table_size . ' WHERE object_id = ' . $ship_id;
 
-       // echo  $query;
+        // echo  $query;
         $result = $wpdb->get_row($query);
 
         return $result;

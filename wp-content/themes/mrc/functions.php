30e20fc2944c9015103a660ba053b7205f4476ac
dao vs <vosydao88@gmail.com>
2016-07-14 19:48:37 +0700

    new update

--- wp-content/themes/mrc/functions.php
+++ wp-content/themes/mrc/functions.php
@@ -16,26 +16,16 @@ add_action('after_setup_theme', 'r_theme_setup');
 function r_scripts_styles() {
     $version = '20150116_1200';
 
+    wp_enqueue_style('plugin-css', THEME_URL . '/css/plugin.css', array(), $version);
+    wp_enqueue_style('em-style-ext', THEME_URL . '/style.css', array(), $version);
 
-    wp_enqueue_script('my-app-ext', THEME_URL . '/js/app-ext.js', array('jquery'), $version, true);
+    wp_enqueue_script('plugins-js', THEME_URL . '/js/plugins.js', array('jquery'), $version, true);
+    wp_enqueue_script('my-app-ext', THEME_URL . '/js/main.js', array('jquery'), $version, true);
     wp_localize_script( 'my-app-ext', 'MyAjax', array( 'ajax_url' => admin_url( 'admin-ajax.php' )));
 
 }
 
 add_action('wp_enqueue_scripts', 'r_scripts_styles');
-
-function r_widgets_init() {
-    register_sidebar( array(
-        'name'          => 'Block meta tag',
-        'id'            => 'block-meta-tag',
-        'description'   => 'Block các thẻ meta tại sidebar',
-        'before_widget' => '<div id="%1$s" class="%2$s">',
-        'after_widget'  => '</div>',
-        'before_title'  => '',
-        'after_title'   => '',
-    ) );
-}
-add_action( 'widgets_init', 'r_widgets_init' );
 
 
 add_action('after_setup_theme', 'remove_admin_bar');

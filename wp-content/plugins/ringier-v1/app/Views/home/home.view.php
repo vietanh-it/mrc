0c34da10df3c309d91407ebff2dae9c01b25a564
dao vs <vosydao88@gmail.com>
2016-07-16 12:09:23 +0700

    new update

--- wp-content/plugins/ringier-v1/app/Views/home/home.view.php
+++ wp-content/plugins/ringier-v1/app/Views/home/home.view.php
@@ -14,10 +14,42 @@ get_header();
         <div><img src="<?php echo THEME_URL.'/images/bn3.jpg' ?>" alt=""></div>
         <div><img src="<?php echo THEME_URL.'/images/bn4.jpg' ?>" alt=""></div>
     </div>
+    
+    <div class="container">
+        <div class="form-find-journey">
+            <form>
+                <h3>Find your journey</h3>
+                <div class="form-group">
+                    <select name="destination" class="form-control">
+                        <option value="">Choose your destination</option>
+                    </select>
+                    <span class="icon-n icon-location"></span>
+                </div>
+                <div class="form-group">
+                    <input type="text" name="month" class="form-control" placeholder="Choose sail month">
+                    <span class="icon-n icon-date"></span>
+                </div>
+                <div class="form-group">
+                    <select name="port" class="form-control">
+                        <option value="">Choose port of departure</option>
+                    </select>
+                    <span class="icon-n icon-port"></span>
+                </div>
 
-    <div class="form-booking">
-        <form></form>
+                <div class="form-group">
+                    <select name="ship" class="form-control">
+                        <option value="">Choose your ship</option>
+                    </select>
+                    <span class="icon-n icon-ship"></span>
+                </div>
+
+                <div class="text-center">
+                    <button type="submit"> <i class="fa fa-search" aria-hidden="true"></i> Find now</button>
+                </div>
+            </form>
+        </div>
     </div>
+    
 </div>
 
 

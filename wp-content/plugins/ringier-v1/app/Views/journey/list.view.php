a7478aa7760e58218bc683b4f478d3dde0f5c6f3
dao vs <vosydao88@gmail.com>
2016-07-18 15:27:19 +0700

    new update

--- wp-content/plugins/ringier-v1/app/Views/journey/list.view.php
+++ wp-content/plugins/ringier-v1/app/Views/journey/list.view.php
@@ -6,61 +6,9 @@
  * Time: 2:49 PM
  */
 get_header();
+
+echo   view('journey/quick-search');
 ?>
-
-<div class="quick-search-journey">
-    <form>
-        <div class="container container-big">
-            <div class="row">
-                <h3 class="col-xs-12 col-sm-12">Find your journey</h3>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 col-sm-10">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-3">
-                            <div class="form-group">
-                                <select name="destination" class="form-control">
-                                    <option value="">Choose your destination</option>
-                                </select>
-                                <span class="icon-n icon-location"></span>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-3">
-                            <div class="form-group">
-                                <input type="text" name="month" class="form-control" placeholder="Choose sail month">
-                                <span class="icon-n icon-date"></span>
-                            </div>
-                        </div>
-
-                        <div class="col-xs-12 col-sm-3">
-                            <div class="form-group">
-                                <select name="port" class="form-control">
-                                    <option value="">Choose port of departure</option>
-                                </select>
-                                <span class="icon-n icon-port"></span>
-                            </div>
-                        </div>
-
-                        <div class="col-xs-12 col-sm-3">
-                            <div class="form-group">
-                                <select name="ship" class="form-control">
-                                    <option value="">Choose your ship</option>
-                                </select>
-                                <span class="icon-n icon-ship"></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xs-12 col-sm-2">
-                    <div class="text-center">
-                        <button type="submit"> <i class="fa fa-search" aria-hidden="true"></i> Find now</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </form>
-</div>
 
 <div class="container">
     <div class="row">

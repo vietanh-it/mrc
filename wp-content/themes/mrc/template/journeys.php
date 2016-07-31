c6f13df51b46abf9c63bafbcccede51759b5b39c
Viet Anh <vietanh.sgu@gmail.com>
2016-08-01 02:02:46 +0700

    update

--- wp-content/themes/mrc/template/journeys.php
+++ wp-content/themes/mrc/template/journeys.php
@@ -3,10 +3,12 @@
  * Template name: Journey List
  */
 
-if(empty($_GET['_month'])){
+$get_params = $_GET;
+unset($get_params['q']);
+if (empty($get_params['_month'])) {
     $journey_ctrl = \RVN\Controllers\JourneyTypeController::init();
-    $journey_ctrl->journeyTypeList($_GET);
-}else{
+    $journey_ctrl->journeyTypeList($get_params);
+} else {
     $journey_ctrl = \RVN\Controllers\JourneyController::init();
-    $journey_ctrl->journeyList($_GET);
+    $journey_ctrl->journeyList($get_params);
 }

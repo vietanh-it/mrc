0d7eded483250d69f94db03d775e6f83cf39cffb
dao vs <vosydao88@gmail.com>
2016-08-05 15:09:32 +0700

    a

--- wp-content/plugins/ringier-v1/app/Views/home/home.view.php
+++ wp-content/plugins/ringier-v1/app/Views/home/home.view.php
@@ -127,17 +127,17 @@ get_header();
                                 ?>
                                 <div class="col-xs-12 col-sm-12">
                                     <div class="box-journey box-white">
-                                        <div class="image">
+                                        <div class="image" style="position: relative">
                                             <a href="<?php echo $v->permalink ?>" title="<?php echo $v->post_title ?>">
                                                 <img src="<?php echo $v->images->small ?>" alt="<?php echo $v->post_title ?>" class="lazy">
                                             </a>
+                                            <div class="price">  $<?php echo number_format($v->journey_info->min_price_offer) ?></div>
                                         </div>
                                         <div class="desc">
                                             <a href="<?php echo $v->permalink ?>" class="title" title="<?php echo $v->post_title ?>"><?php echo $v->post_title ?></a>
                                             <p><?php echo cut_string_by_char(strip_tags($v->post_content),150) ?></p>
                                             <p><b>Start Date:</b> <?php echo  date("j F Y", strtotime($v->start_date)); ?> <a href="<?php echo $v->permalink ?>" class="read-more" title="read more"><i class="fa fa-angle-right" aria-hidden="true"></i></a> </p>
                                         </div>
-                                        <div class="price">  $<?php echo number_format($v->journey_info->min_price_offer) ?></div>
                                         <div class="star"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i></div>
                                     </div>
                                 </div>

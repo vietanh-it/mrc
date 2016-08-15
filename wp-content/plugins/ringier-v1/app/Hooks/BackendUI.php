a9048508d9422a0f38f81d51d7dd92049c5295df
Viet Anh <vietanh.sgu@gmail.com>
2016-08-15 22:46:40 +0700

    update

--- wp-content/plugins/ringier-v1/app/Hooks/BackendUI.php
+++ wp-content/plugins/ringier-v1/app/Hooks/BackendUI.php
@@ -52,9 +52,15 @@ class BackendUI
     public function adminMenu()
     {
         remove_menu_page('link-manager.php');
-        // remove_menu_page('tools.php');
-        // remove_menu_page('themes.php');
-        // remove_menu_page('plugins.php');
+        if (!current_user_can('administrator')) {
+            remove_menu_page('tools.php');
+            remove_menu_page('themes.php');
+            remove_menu_page('plugins.php');
+            remove_menu_page('edit.php?post_type=page');
+            remove_menu_page('edit-comments.php');
+            remove_menu_page('edit.php');
+        }
+
         // remove_menu_page('upload.php');
         // remove_menu_page('edit.php?post_type=acf');
     }

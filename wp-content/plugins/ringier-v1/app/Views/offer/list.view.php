0ad8f3b076818377632c82982f70b7006136d676
dao vs <vosydao88@gmail.com>
2016-08-17 12:43:54 +0700

    a

--- wp-content/plugins/ringier-v1/app/Views/offer/list.view.php
+++ wp-content/plugins/ringier-v1/app/Views/offer/list.view.php
@@ -15,10 +15,11 @@ $list_offer = $list_offer ? $list_offer : array(); ?>
                         ?>
                         <div class="col-xs-12 col-sm-4">
                             <div class="box-journey box-white">
-                                <div class="image">
+                                <div class="image" style="position: relative">
                                     <a href="<?php echo $v->permalink ?>" title="<?php echo $v->post_title ?>">
                                         <img src="<?php echo $v->images->small ?>" alt="<?php echo $v->post_title ?>" class="lazy">
                                     </a>
+                                    <div class="price">  $<?php echo number_format($v->journey_info->min_price_offer) ?></div>
                                 </div>
                                 <div style="border: 1px solid #ccc">
                                     <div class="desc">
@@ -26,7 +27,6 @@ $list_offer = $list_offer ? $list_offer : array(); ?>
                                         <p><?php echo cut_string_by_char(strip_tags($v->post_content),150) ?></p>
                                         <p><b>Start Date:</b> <?php echo  date("j F Y", strtotime($v->start_date)); ?> <a href="<?php echo $v->permalink ?>" class="read-more" title="read more"><i class="fa fa-angle-right" aria-hidden="true"></i></a> </p>
                                     </div>
-                                    <div class="price">  $<?php echo number_format($v->journey_info->min_price_offer) ?></div>
                                     <div class="star"><i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i></div>
                                 </div>
                             </div>

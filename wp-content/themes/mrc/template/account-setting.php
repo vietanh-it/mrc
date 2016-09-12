3a2a3194b24e0c161f900e35054e56899eca9fed
dao vs <vosydao88@gmail.com>
2016-09-12 13:43:14 +0700

    âs

--- wp-content/themes/mrc/template/account-setting.php
+++ wp-content/themes/mrc/template/account-setting.php
@@ -3,6 +3,9 @@
  * Template name: Account setting
  */
 
-if(!is_user_logged_in()) header('#');
+if (!is_user_logged_in()) {
+    wp_redirect(wp_login_url());
+    exit;
+}
 $ctr = \RVN\Controllers\Account\AccountController::init();
 $ctr->userInfo(get_current_user_id());

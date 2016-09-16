df1306d011459f6216764e0b016bb3d9903c5793
vietanh <vietanh.sgu@gmail.com>
2016-09-17 00:42:34 +0700

    update

--- wp-content/themes/mrc/page.php
+++ wp-content/themes/mrc/page.php
@@ -6,7 +6,7 @@ while (have_posts()) : the_post(); ?>
             <h1 class="col-xs-12 col-sm-12 tile-main"><?php the_title() ?>
                 <br> <img src="<?php echo VIEW_URL . '/images/line.png' ?>">
             </h1>
-            <div class="col-xs-12 col-sm-10 col-sm-offset-1">
+            <div class="col-xs-12 col-sm-10 col-sm-offset-1" style="margin-bottom: 50px;">
                 <?php the_content() ?>
             </div>
         </div>

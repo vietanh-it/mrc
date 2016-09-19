373c4948cf892b1130c9467bd6248c6ab032369d
vietanh <vietanh.sgu@gmail.com>
2016-09-19 23:17:43 +0700

    update

--- wp-content/plugins/ringier-v1/app/Views/home/home.view.php
+++ wp-content/plugins/ringier-v1/app/Views/home/home.view.php
@@ -267,7 +267,7 @@ get_header();
                                     <div class="col-xs-12 col-sm-3">
                                         <div class="box-room">
                                             <?php foreach ($room->gallery as $kg =>  $img){ ?>
-                                                <a <?php echo $kg != 0 ? "style ='display:none'" : '' ?> href="<?php echo $img->full ?>" title="<?php echo $img->caption ?>" class="fancybox" rel="list_room_<?php echo $room->caption ?>">
+                                                <a <?php echo $kg != 0 ? "style ='display:none'" : '' ?> href="<?php echo $img->full ?>" title="<?php echo $img->caption ?>" class="fancybox" rel="list_room_<?php echo $img->caption ?>">
                                                     <img src="<?php echo $img->featured ?>" alt="" class="lazy" >
                                                 </a>
                                             <?php } ?>

ac0f769e372b34a826c2b5dd3ef93ac5caeeabc0
dao vs <vosydao88@gmail.com>
2016-09-21 22:29:28 +0700

    âs

--- wp-content/plugins/ringier-v1/app/Controllers/Home/HomeController.php
+++ wp-content/plugins/ringier-v1/app/Controllers/Home/HomeController.php
@@ -50,13 +50,29 @@ class HomeController extends _BaseController
         $list_offer = $objOffer->getListOffer(array());
 
         $Post = Posts::init();
+        $list_room_items_featured = $Post->getList(array(
+            'post_type' => 'album',
+            'post_status' => 'publish',
+            'meta_key' => 'is_featured',
+            'meta_value' => '1',
+            'posts_per_page' => 1,
+        ));
+
+        $not_in = 0;
+        $limit_room = 5;
+        if(!empty($list_room_items_featured['data'])){
+            $limit_room = 4;
+            $not_in = $list_room_items_featured['data'][0]->ID;
+        }
+
         $list_room_items = $Post->getList(array(
             'post_type' => 'album',
             'post_status' => 'publish',
-            'posts_per_page' => 5,
+            'posts_per_page' => $limit_room,
+            'post__not_in' => array($not_in),
         ));
 
-        return view('home/home',compact('list_destination','list_ship','list_port','list_journey_type','list_offer','list_month','list_room_items'));
+        return view('home/home',compact('list_destination','list_ship','list_port','list_journey_type','list_offer','list_month','list_room_items','list_room_items_featured'));
     }
 }
 

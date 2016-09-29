e401f7567b47fb0195cbcd461988763f341a82dc
dao vs <vosydao88@gmail.com>
2016-09-29 15:44:38 +0700

    âs

--- wp-content/plugins/ringier-v1/app/Views/page/qa.view.php
+++ wp-content/plugins/ringier-v1/app/Views/page/qa.view.php
@@ -5,7 +5,7 @@ $list_qa = !empty($list_qa) ? $list_qa : array();
 
 <div class="container">
     <div class="row">
-        <h1 class="col-xs-12 col-sm-12 tile-main"> Q&A
+        <h1 class="col-xs-12 col-sm-12 tile-main"> FAQ
             <br> <img src="<?php echo VIEW_URL . '/images/line.png' ?>">
         </h1>
         <div class="col-xs-12 col-sm-10 col-sm-offset-1">

0b5f28a9f26fa99fe82a1df658cc791d31ed8c6e
vietanh <vietanh.sgu@gmail.com>
2016-11-27 13:14:43 +0700

    update

--- wp-content/plugins/ringier-v1/app/Views/header.view.php
+++ wp-content/plugins/ringier-v1/app/Views/header.view.php
@@ -23,13 +23,9 @@
     <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
     <?php wp_head(); ?>
     <script>
-        /*(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-         ga('create', 'UA-62562895-1', 'auto');
-         ga('send', 'pageview');*/
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-87810165-1', 'auto');
+        ga('send', 'pageview');
     </script>
     <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
     <script src="https://apis.google.com/js/client:platform.js?onload=start" async defer></script>
